2d129298baaa49cf9bb7fcebff392e98daf1011f
heesoo <11junghs@gmail.com>
2023-01-03 14:47:39 +0900

    feat : ourpick 기능 추가

--- src/pages/recipe/ourpick.tsx
+++ src/pages/recipe/ourpick.tsx
@@ -1,3 +1,35 @@
+import RecipeList from "../../components/recipe/RecipeList"
+import colors from "../../../styles"
+import MiniHeader from "../../components/common/MiniHeader"
+
 export default function OurPick(){
-    
+    return(
+        <>
+        <div className='miniHeader'>
+            <MiniHeader left="추천 식사" right="Our Pick!" left_url="/recipe" right_url="/recipe/ourpick"/>
+        </div>
+        <div className="container">
+            가족 모두의 PICK! <br/>
+            <RecipeList type="top"/>
+        </div>
+        <div className="bar" />
+        <div className="container">
+            가족 내 인기 PICK!
+            <RecipeList type="bottom"/>
+        </div>
+        <style jsx>{`
+            .container {
+                margin-left: 20px;
+                margin-right: 20px;
+            }
+            .bar {
+                background: ${colors.grayBackgroundSub};
+                height: 8px;
+                width: 390px;
+
+                margin-bottom: 18px;
+            }
+        `}</style>
+        </>
+    )
 }
