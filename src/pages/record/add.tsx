3edeb303b69cb2ed6165a9f6eefe9a699e0efa1c
heesoo <11junghs@gmail.com>
2023-01-04 18:00:26 +0900

    feat : toggle 추가

--- src/pages/record/add.tsx
+++ src/pages/record/add.tsx
@@ -1,18 +1,34 @@
 import Navigation from "../../components/common/Navigation"
 import colors from "../../../styles"
+import { useState } from "react"
 export default function Add(){
+    const [toggle,setToggle] = useState(false);
+
+    const handleClick = () => {
+        setToggle(!toggle);
+    }
     return(
         <>
         <Navigation text="식후 혈당 기록하기"/>
         <div className="container">
             <div className="item">
                 내 식단
+                <button onClick={handleClick}>toggle</button>
             </div>
+            {
+                toggle ?
+                <div className="item_toggle">
+                    주먹을 부르는 주먹밥 22.12.12 아침
+                </div>
+                :
+                null
+            }
             <div className="item">
                 날짜
             </div>
             <div className="item">
                 시간
+                <input type="time"/>
             </div>
             <div className="item">
                 혈당
@@ -32,6 +48,28 @@ export default function Add(){
                 margin-right: 20px;
                 margin-bottom: 12px;
                 border-radius: 6px;
+
+                display: flex;
+                align-items: center;
+            }
+
+            .item_toggle {
+                width: 340px;
+                height: 53px;
+                background: none;
+
+                margin-left: 20px;
+                margin-right: 20px;
+                margin-bottom: 12px;
+                border-radius: 6px;
+                border: 1px solid ${colors.graySubTitle2}; 
+
+                display: flex;
+                align-items: center;
+            }
+
+            input[type="time"] {
+                float: right;
             }
         `}</style>
         </>
