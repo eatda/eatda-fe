c31703b21de570e66c29f5c1ee17915a896bcda6
heesoo <11junghs@gmail.com>
2023-01-04 17:30:39 +0900

    feat : 식후 혈당 기록하기 페이지 추가

--- src/pages/record/add.tsx
+++ src/pages/record/add.tsx
@@ -1,7 +1,39 @@
+import Navigation from "../../components/common/Navigation"
+import colors from "../../../styles"
 export default function Add(){
     return(
         <>
-        식후혈당 기록하기
+        <Navigation text="식후 혈당 기록하기"/>
+        <div className="container">
+            <div className="item">
+                내 식단
+            </div>
+            <div className="item">
+                날짜
+            </div>
+            <div className="item">
+                시간
+            </div>
+            <div className="item">
+                혈당
+            </div>
+        </div>
+        <style jsx>{`
+            .container {
+                width: 390px;
+                padding-top: 70px;
+            }
+            .item {
+                width: 340px;
+                height: 53px;
+                background: ${colors.grayWhite};
+
+                margin-left: 20px;
+                margin-right: 20px;
+                margin-bottom: 12px;
+                border-radius: 6px;
+            }
+        `}</style>
         </>
     )
 }
