a94450f2499310b78d523c6375d447b098be75a9
seondal <sseondal@gmail.com>
2023-01-07 11:42:10 +0900

    ✨ feat :  메인화면 슬라이더 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,15 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  spped: 500,
+  slideToShow: 1,
+  slideToScroll: 1,
+};
+
 export default function Home() {
   return (
     <>
@@ -8,7 +20,17 @@ export default function Home() {
         </div>
         <div className="homeBox">
           <h4>오늘의 식사</h4>
-          <div>카드</div>
+          <Slider {...sliderSettings}>
+            <div>
+              <h3>아침</h3>
+            </div>
+            <div>
+              <h3>점심</h3>
+            </div>
+            <div>
+              <h3>저녁</h3>
+            </div>
+          </Slider>
         </div>
         <div className="homeBox">
           <h4>오늘의 식후 혈당</h4>
