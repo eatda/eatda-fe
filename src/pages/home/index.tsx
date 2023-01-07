da449268a3b076ee6c2ae09f264dcc062cc16339
seondal <sseondal@gmail.com>
2023-01-07 11:15:23 +0900

    💄 style :  홈화면 레이아웃 구성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,31 @@
 export default function Home() {
   return (
     <>
-      <div>홈</div>
+      <div>
+        <div className="header">
+          <h2>Eat Da</h2>
+          <button>character</button>
+        </div>
+        <div className="homeBox">
+          <h4>오늘의 식사</h4>
+          <div>카드</div>
+        </div>
+        <div className="homeBox">
+          <h4>오늘의 식후 혈당</h4>
+          <div>카드</div>
+        </div>
+        <div className="homeBox">
+          <h4>주간 혈당 요약</h4>
+          <div>카드</div>
+        </div>
+      </div>
+      <style jsx>{`
+        .header {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+      `}</style>
     </>
   );
 }
