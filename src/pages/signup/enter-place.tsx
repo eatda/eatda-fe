a202fc58b35160a7adac589ad000df3478849d99
heesoo <11junghs@gmail.com>
2023-01-10 02:33:20 +0900

    feat : enter place 코드 한자씩 입력

--- src/pages/signup/enter-place.tsx
+++ src/pages/signup/enter-place.tsx
@@ -1,36 +1,45 @@
+import React, { useEffect, useRef, useState } from "react";
 import colors from "../../../styles";
 
 export default function EnterPlace(){
+    const inputRef = useRef<null[] | HTMLDivElement[]>([]);
+    const idxArray = Array.from({length: 6}, (v,i) => i);
+
+    useEffect(()=>{
+        inputRef.current[0]?.focus()
+    },[])
+
+    const handleTest = (e:React.ChangeEvent<HTMLInputElement>,idx:number) => {
+        inputRef.current[++idx]?.focus()
+    }
+
+
     return(
         <div className="container">
         <div className="subText">
             가족 구성원이 이미 가입하셨군요!
         </div>
             초대 코드를 입력해주세요 
-            <input />
+            <br/>
+            {
+                idxArray.map((v,i)=>{
+                    return (
+                    <input key={i} maxLength={1} ref={(el) => (inputRef.current[i] = el)} onChange={(e)=>handleTest(e,i)}/>
+                    )
+                })
+            }
             <style jsx>{`
             .container {
                 width: 390px;
                 padding-top: 60px;
                 height: 100vh;
             } 
-            .buttonItem {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 60px;
-                position: fixed;
-                bottom: 36px;
-                left: 0;
-                right: 0;
-            }
-            .groudId {
-                width: 350px;
-                height: 134px;
-                background: ${colors.grayWhite};
-            }
             .subText {
                 color: ${colors.graySubTitle};
+            }
+
+            input {
+                width: 30px;
             }
         `}</style>
         </div>
