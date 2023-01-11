6a5091b82b31283fcc1412827cb9f9bfc10ba6e0
heesoo <11junghs@gmail.com>
2023-01-11 14:10:49 +0900

    feat : 코드 한 자씩 수정 구현

--- src/pages/signup/enter-place.tsx
+++ src/pages/signup/enter-place.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useRef, useState } from "react";
 import colors from "../../../styles";
+import { CTA1Button } from "../../components/common/Button";
 
 export default function EnterPlace(){
+    const codeLength = 6;
     const inputRef = useRef<null[] | HTMLDivElement[]>([]);
-    const idxArray = Array.from({length: 6}, (v,i) => i);
+    const idxArray = Array.from({length: codeLength}, (v,i) => i);
+
+    const [test, setTest] = useState(()=>Array(codeLength).fill(0));
 
     useEffect(()=>{
         inputRef.current[0]?.focus()
     },[])
 
-    const handleTest = (e:React.ChangeEvent<HTMLInputElement>,idx:number) => {
-        inputRef.current[++idx]?.focus()
+    const handleFocus = (e:React.ChangeEvent<HTMLInputElement>,idx:number) => {
+        let arr = test;
+        arr.splice(idx,1,e.target.value);
+        setTest(arr);
+        inputRef.current[++idx]?.focus();
+    }
+
+    const handleClick = () => {
+        console.log(test);
     }
 
 
@@ -24,10 +35,17 @@ export default function EnterPlace(){
             {
                 idxArray.map((v,i)=>{
                     return (
-                    <input key={i} maxLength={1} ref={(el) => (inputRef.current[i] = el)} onChange={(e)=>handleTest(e,i)}/>
+                    <input 
+                    key={i} 
+                    maxLength={1} 
+                    ref={(el) => (inputRef.current[i] = el)} 
+                    onChange={(e)=>handleFocus(e,i)}
+                    />
                     )
                 })
             }
+            <br/>
+            <CTA1Button text="입장" active={true} onClick={handleClick}/>
             <style jsx>{`
             .container {
                 width: 390px;
