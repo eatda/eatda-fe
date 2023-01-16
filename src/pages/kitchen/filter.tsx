00a5324cabfe9136b5b1d85ea5a3df3a6e729d93
seondal <sseondal@gmail.com>
2023-01-16 18:22:34 +0900

    ✨ feat :  필터리스트 불러오기 api 연결

--- src/pages/kitchen/filter.tsx
+++ src/pages/kitchen/filter.tsx
@@ -2,57 +2,22 @@ import { useEffect, useState } from "react";
 import Navigation from "../../components/common/Navigation";
 import FilterButton from "../../components/filter/FilterButton";
 
-export interface FilterType {
+interface FilterType {
   id: number;
   name: string;
-  filter_category_id: number;
+  category: number;
+  image: string;
   selected?: boolean;
 }
 interface FilterDataType {
-  filter_category: { id: number; name: string };
+  category: { id: number; name: string };
   filter: FilterType[];
 }
+interface FilterProps {
+  filterData: FilterDataType[];
+}
 
-const FilterData: FilterDataType[] = [
-  {
-    filter_category: {
-      id: 0,
-      name: "음식 종류",
-    },
-    filter: [
-      {
-        id: 0,
-        name: "한식",
-        filter_category_id: 1,
-      },
-      {
-        id: 1,
-        name: "중식",
-        filter_category_id: 1,
-      },
-    ],
-  },
-  {
-    filter_category: {
-      id: 1,
-      name: "맛",
-    },
-    filter: [
-      {
-        id: 4,
-        name: "매콤",
-        filter_category_id: 2,
-      },
-      {
-        id: 5,
-        name: "달콤",
-        filter_category_id: 2,
-      },
-    ],
-  },
-];
-
-export default function Filter() {
+export default function Filter({ filterData }: FilterProps) {
   const [selectedFilter, setSelectedFilter] = useState<Set<FilterType>>(
     new Set()
   );
@@ -72,11 +37,11 @@ export default function Filter() {
       <div className="container">
         <Navigation text={"필터"} />
         <div className="content">
-          {FilterData.map((category) => (
-            <div key={category.filter_category.id} className="category">
-              <h2>{category.filter_category.name}</h2>
+          {filterData.map((data: FilterDataType) => (
+            <div key={data.category.id} className="category">
+              <h2>{data.category.name}</h2>
               <div className="filters">
-                {category.filter.map((filter) => (
+                {data.filter.map((filter) => (
                   <FilterButton
                     key={filter.id}
                     filter={filter}
@@ -97,3 +62,14 @@ export default function Filter() {
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const filterData = await (
+    await fetch(`${process.env.NEXT_PUBLIC_API_ROOT}diets/filter`)
+  ).json();
+  return {
+    props: {
+      filterData,
+    },
+  };
+};
