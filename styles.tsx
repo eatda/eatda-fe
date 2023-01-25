106de0dab32ed3697ea41b23e00d58d8c5d67b1e
seondal <sseondal@gmail.com>
2023-01-26 02:56:10 +0900

    📦 add :  colors.blackSub (#E6E6E6)

--- styles.tsx
+++ styles.tsx
@@ -1,18 +1,20 @@
 const colors = {
-    mainOrange: '#FF9458',
-    mainYellow: '#FFE094',
+  mainOrange: "#FF9458",
+  mainYellow: "#FFE094",
 
-    grayMainTitle: '#000000',
-    grayTextBlack: '#323232',
-    graySubTitle: '#6D727E',
-    graySubTitle2: '#B1B6C2',
-    grayBackgroundSub: '#E5E7EB',
-    grayBackground: '#F5F6FA',
-    grayWhite: '#FFFFFF',
+  grayMainTitle: "#000000",
+  grayTextBlack: "#323232",
+  graySubTitle: "#6D727E",
+  graySubTitle2: "#B1B6C2",
+  grayBackgroundSub: "#E5E7EB",
+  grayBackground: "#F5F6FA",
+  grayWhite: "#FFFFFF",
 
-    subGreen: '#DAEABC',
-    subRed: '#DD7259',
-    subPink: '#F2A9A7'
-}
+  subGreen: "#DAEABC",
+  subRed: "#DD7259",
+  subPink: "#F2A9A7",
+
+  blackSub: "#E6E6E6",
+};
 
 export default colors;
