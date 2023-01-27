fe2b44fdbbc7f212d134552db5b207a0a2431dfa
seondal <sseondal@gmail.com>
2023-01-28 05:06:49 +0900

    💄 style :  Navigation

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -11,11 +11,16 @@ export default function Navigation({ text }: NavigationProps) {
     <>
       <div className="container">
         <button onClick={() => router.back()}>
-          <Image alt="back" width={32} height={32} src={`/button/back.svg`} priority/>
+          <Image
+            alt="back"
+            width={32}
+            height={32}
+            src={`/button/back.svg`}
+            priority
+          />
         </button>
-        <div className="text">
-          {text}
-        </div>
+        <div className="text">{text}</div>
+        <div className="blank"></div>
       </div>
       <style jsx>{`
         .container {
@@ -23,22 +28,25 @@ export default function Navigation({ text }: NavigationProps) {
           display: flex;
           flex-direction: row;
           align-items: center;
-          justify-content: center;
+          justify-content: space-between;
           position: fixed;
           top: 0;
           left: 0;
           right: 0;
+          background-color: white;
         }
         button {
-          // float: left;
           border: none;
-          // margin-right: auto; 
           background: none;
         }
-        text {
+        .text {
           font-size: 18px;
           font-weight: 500;
           text-align: center;
+        }
+        .blank {
+          width: 44px;
+          height: 44px;
         }
       `}</style>
     </>
