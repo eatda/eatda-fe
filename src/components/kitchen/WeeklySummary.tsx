79f04615802f59cea6f3c535e132e6fe937a1b67
seondal <sseondal@gmail.com>
2023-02-02 03:15:28 +0900

    💄 style :  주간 혈당 요약 디자인 작업

--- src/components/kitchen/WeeklySummary.tsx
+++ src/components/kitchen/WeeklySummary.tsx
@@ -14,54 +14,90 @@ export default function WeeklySumary({
   high,
   data,
 }: WeeklySummaryProps) {
+  const sugarLevelList = [
+    { color: colors.mainYellow, text: "저혈당", value: low },
+    { color: colors.mainOrange, text: "정상혈당", value: common },
+    { color: colors.subRed, text: "고혈당", value: high },
+  ];
+
   return (
     <>
-      <div className="summary">
-        <div className="summary-list">
-          <div className="summary-item">저혈당 {low}일</div>
-          <div className="summary-item">정상혈당 {common}일</div>
-          <div className="summary-item">고혈당 {high}일</div>
+      <div className="container">
+        <div className="ul">
+          {sugarLevelList.map((type) => (
+            <div className="li">
+              <div className="circle" style={{ color: type.color }}>
+                ●
+              </div>
+              {type.text} <div className="value">{type.value}일</div>
+            </div>
+          ))}
         </div>
-        <div className="bar-graph">
+        <div className="graph">
           {data.map((day, idx) => (
-            <div key={idx} className={`bar-item level${day.level}`}>
-              {day.day}
+            <div key={idx} className={`item level${day.level}`}>
+              <div className="text"> {day.level !== 0 && day.day}</div>
             </div>
           ))}
         </div>
       </div>
       <style jsx>{`
-        .summary {
+        .container {
           background-color: ${colors.grayBackground};
+          padding: 12px;
+          font-weight: 700;
         }
-        .summary-list {
+
+        // 저혈당 고혈당 수
+        .ul {
           display: flex;
           flex-direction: row;
           justify-content: space-between;
+          margin-bottom: 5px;
         }
-        .bar-graph {
+        .li {
+          display: flex;
+          flex-direction: row;
+        }
+        .circle {
+          margin-right: 5px;
+        }
+        .value {
+          margin-left: 4px;
+          font-size: 12px;
+          color: ${colors.graySubTitle};
+        }
+
+        // 그래프
+        .graph {
           display: flex;
           flex-direction: row;
           justify-content: space-between;
           gap: 6px;
         }
-        .bar-item {
+        .item {
+          height: 24px;
           display: flex;
           flex: 1;
           justify-content: center;
+          align-items: center;
+          border-radius: 2px;
+        }
+        .text {
           color: ${colors.grayWhite};
+          opacity: 0.3;
         }
         .level0 {
           border: solid 1px ${colors.blackSub};
         }
         .level1 {
-          background-color: ${colors.mainYellow};
+          background-color: ${sugarLevelList[0].color};
         }
         .level2 {
-          background-color: ${colors.mainOrange};
+          background-color: ${sugarLevelList[1].color};
         }
         .level3 {
-          background-color: ${colors.subRed};
+          background-color: ${sugarLevelList[2].color};
         }
       `}</style>
     </>
