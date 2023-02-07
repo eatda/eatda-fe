2047d04a5c20f31ea48584408caa8245a2289f2e
seondal <sseondal@gmail.com>
2023-02-07 18:42:52 +0900

    ✨ feat :  pwa 아이폰 안전영역 대응

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-512.png" />
         <meta name="theme-color" content="#FFFFFF" />
+        <link rel="apple-touch-icon" href="/icons/icon-512.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
       </Head>
       <body>
         <Main />
